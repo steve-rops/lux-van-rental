4921bc224d166d6c5d86b3e06f2117fcb5ed1a3c
steve-rops <somebiss@gmail.com>
2025-03-09 00:26:39 +0200

    added the keep-alive for supabase with GH actions

---
+++ app/api/users/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  console.log(users);
+  return NextResponse.json(users);
+}
